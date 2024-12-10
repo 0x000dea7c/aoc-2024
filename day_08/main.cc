661d0d17aae0e1a85a760334b2717c3039e7b07f
whocares <flevn@protonmail.com>
2024-12-10 23:14:11 +0100

    WIP day 08 part2

--- day_08/main.cc
+++ day_08/main.cc
@@ -16,25 +16,39 @@ inline int get_key(int i, int j) {
 
 inline static void
 place_antinodes(std::unordered_set<int>& antinodes, std::pair<int, int> const& a1, std::pair<int, int> const& a2, int m, int n) {
-  // grab the direction vector, do not normalise it bc otherwise i'm cooked
-  // not normalising is good bc it allows me to jump to the right coordinate!
+  // WIP: i know i need to normalise the direction vector... but this is all half assed rn bc my brain
+  // refuses to work
   int dr = a2.first - a1.first;
   int dc = a2.second - a1.second;
-  int rup = a1.first - dr;
-  int cup = a1.second - dc;
-  int rdown = a2.first + dr;
-  int cdown = a2.second + dc;
-  if (rup >= 0 && rup < m && cup >= 0 && cup < n) { // inside matrix
+  double magnitude = sqrt(dr * dr + dc * dc); // sqrt(y² + x²)
+  assert(magnitude != 0.0);
+  std::cout << "magnitude = " << magnitude << std::endl;
+  std::cout << "dr = " << dr << std::endl;
+  int dr_round = dr / round(magnitude);
+  int dc_round = dc / round(magnitude);
+  assert(dr_round != 0);
+  assert(dc_round != 0);
+  int rup = a1.first - dr_round;
+  int cup = a1.second - dc_round;
+  int rdown = a2.first + dr_round;
+  int cdown = a2.second + dc_round;
+  while (rup >= 0 && rup < m && cup >= 0 && cup < n) { // inside matrix
+    //std::cout << "adding up: (" << rup << ", " << cup << ")" << std::endl;
     antinodes.emplace(get_key(rup, cup));
+    rup -= dr_round;
+    cup -= dc_round;
   }
-  if (rdown >= 0 && rdown < m && cdown >= 0 && cdown < n) { // inside matrix
+  while (rdown >= 0 && rdown < m && cdown >= 0 && cdown < n) { // inside matrix
+    //std::cout << "adding down: (" << rdown << ", " << cdown << ")" << std::endl;
     antinodes.emplace(get_key(rdown, cdown));
+    rdown += dr_round;
+    cdown += dc_round;
   }
 }
 
 int
 main () {
-  // 25,905ns
+  // 25,905ns (i don't know how part1 worked tbh... i think my impl was wrong)
   std::vector<std::string> lines;
   std::unordered_map<int, std::vector<std::pair<int, int>>> antennas;
   {
